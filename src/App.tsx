82ea404bc34cb592f1ef9f61d5d1b33b50d07cb4
Lightbuild <893458528@qq.com>
2022-09-30 11:47:06 +0800

    引入styled和router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+
+const App =() =>{
+  return(
+    <Routes>
+      <Route path='/' element={ <Home/>}></Route>
+    </Routes>
+  )
+}
+function Home(){
+  return <h2>hello world!</h2>
 }
 
 export default App;
