4b972fffb035a2cf8469117d01c13a32bc6df1ba
Lightbuild <893458528@qq.com>
2022-10-09 17:31:36 +0800

    完成备注功能

--- src/components/notesSection/notesSection.component.tsx
+++ src/components/notesSection/notesSection.component.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, {useRef, useState} from 'react';
 import {NotesContainer} from './notesSecton.styles';
 
 const NotesSection = () =>{
+  const [note,setNote] = useState('');
+  const refInput = useRef<HTMLInputElement>(null)
+  const onBlur = () =>{
+      if(refInput.current !== null){
+        setNote(refInput.current.value);
+      }
+  }
     return(
       <NotesContainer>
           <label>
               <span>备注</span>
-              <input type="text" placeholder="在这里添加备注"/>
+              <input type="text" placeholder="在这里添加备注"
+                ref={refInput}
+                defaultValue={note}
+                onBlur={onBlur}
+              />
           </label>
       </NotesContainer>
     )
